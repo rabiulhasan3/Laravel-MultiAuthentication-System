3558de23bdae47a4270ed563af7e00dcc419d1f0
rabiulhasan3 <rabiul.fci@gmail.com>
2019-04-23 02:12:44 +0600

    Admin & User Password Change

--- app/Http/Controllers/Admin/ForgotPasswordController.php
+++ app/Http/Controllers/Admin/ForgotPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +30,26 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest:admin');
+    }
+
+        /**
+     * Display the form to request a password reset link.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLinkRequestForm()
+    {
+        return view('admin.passwords.email');
+    }
+
+
+    /*
+     * Get the broker to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    public function broker()
+    {
+        return Password::broker('admins'); // Provider Name
     }
 }
